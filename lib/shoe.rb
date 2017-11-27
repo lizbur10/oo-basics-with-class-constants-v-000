f383155abe750d3da40be6b5463279b3c2e07d72
Liz Burton <liz.burton147@gmail.com>
2017-11-27 12:05:06 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,7 +6,9 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-    BRANDS << brand
+    if BRANDS.detect | brand_name | { brand_name == brand }
+      BRANDS << brand
+    end
   end
 
   def cobble
